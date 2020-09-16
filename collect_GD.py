a787cb4d6295cde0b67574434d48e9aec6173b7a
rex <rexhuang881019@gmail.com>
2020-09-16 17:00:26 +0800

    Amend i-th number

--- collect_GD.py
+++ collect_GD.py
@@ -26,21 +26,24 @@ def train(w, b, x, y, epoch, lr):
         dl_db = np.sum(w*x+b-y) / len(x)
         w = w - dl_dw*lr
         b = b - dl_db*lr
-        print("{i}-th loss:",loss(w, b, x, y))
+        print("{i}-th loss:".format(i=i),loss(w, b, x, y))
     return w, b
 
-    
+
 if __name__ == "__main__":
     f = plt.figure()
     plt.plot(x, y)
     plt.title("initial line to train")
-    f.show()
-
+    u = plt.figure()
+    plt.plot(x, w*x+b)
+    plt.title("before training line")
     w, b = train(w, b, x, y , epoch, lr)
     print(w, b)
     c  = plt.figure()
     plt.plot(w*x+b, y)
     plt.title("training line")
+    f.show()
+    u.show()
     c.show()
     while(True):
         q = input("input q to quick")
