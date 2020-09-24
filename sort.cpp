f88ecaf0c69e7671a60bedc4fc49fa66adfd2628
rex <rexhuang881019@gmail.com>
2020-09-25 03:29:15 +0800

    Amend initial_split function

--- sort.cpp
+++ sort.cpp
@@ -8,67 +8,106 @@
 #include <vector>
 using namespace std;
 
-vector<FILE*> initial_split(FILE * file_in, int block_size, int* count_pages){
+void initial_split(FILE * file_in, int block_size, int* count_pages, vector<FILE*> *pages){
     //load to output temp file and sort
     int data;
     int num_data = 0;
     int arr[block_size];
     bool check = true;
-    vector<FILE*> pages(1);
+    int cc = 0;
+    int i ;
     while(check){
-        int i;
         for(i = 0; i < block_size; i++){
             fscanf(file_in, "%d" , &arr[i]);
             if(feof(file_in) != 0){
+                i++;
                 check = false;
                 break;
             }
         }
+        
+
         sort(arr, arr+i);
-        cout << (*count_pages) << endl;
+        cout << "out_temp_" << (*count_pages)+1 << ".txt has been sorted. " << endl;
         (*count_pages) ++ ;
         string title = "out_temp_" + to_string(*count_pages) + ".txt";
-        pages.resize(*count_pages);
-        pages[*count_pages-1] = fopen(title.c_str(), "w");
-        for(int j  = 0; j < i; j++){
-            fprintf(pages[(*count_pages)-1],"%d" ,arr[j]);
-            if(j != i-1)fprintf(pages[(*count_pages)-1], "\n");
+        (*pages).resize(*count_pages);
+        (*pages)[*count_pages-1] = fopen(title.c_str(), "w+");
+        for(int j = 0; j < i; j++){
+            fprintf((*pages)[(*count_pages)-1],"%d\n" ,arr[j]);
+            //if(j != i-1)fprintf((*pages)[(*count_pages)-1], "\n");
+            cc++;
         }
+        cout << "# of fpirntf " << cc << endl; 
+        if(check==false)break;
     }
-    return pages;
+    for(int i = 0; i < (*count_pages); i++)
+        rewind((*pages)[i]);    
+    // int tmp;
+    // fscanf((*pages)[0], "%d", &tmp);
+    // cout << tmp << endl;
+    cout << "# of lines " << cc << endl;  
+    cout << "finish initial split" << endl;
 }
-void merge_file(FILE *file_out, int block_size, int sum_pages, vector<FILE*>pages){
+void merge_file(FILE *file_out, int block_size, int sum_pages, vector<FILE*>*pages){
     //priority_queue <int, vector<int>, greater<int> > pq(block_size); 
     bool check = true;
     int count_pages = 0; // count the null pages
     int arr[sum_pages];
     int standard = 0; //standard to record the min key value
     int standard_pages; // record the min key index
+    //initialize block 
     for(int i = 0; i < sum_pages; i++){
-        fscanf(pages[i], "%d", &arr[i]);
+        int tmp;
+        fscanf((*pages)[i], "%d", &tmp);
+        arr[i] = tmp;
+        cout << arr[i] << endl;
     }
+    //cout << endl;
+    //do selection sort
     while(count_pages != sum_pages){
+        //cout << count_pages << endl;
         standard = arr[0];
+        standard_pages = 0;
+        //find the min key
         for(int i = 0; i < sum_pages; i++){ 
-            if(standard < arr[i]){
+            if(standard > arr[i]){
                 standard = arr[i];
                 standard_pages = i;
             }
-        }    
+        }
+        //input the min key index value
         int tmp;
-        fscanf(pages[standard_pages], "%d", &tmp);
-        if(feof(pages[standard_pages]) != 0){
+        fscanf((*pages)[standard_pages], "%d", &tmp);
+        arr[standard_pages] = tmp;
+        //cout << tmp << endl;
+        if(feof((*pages)[standard_pages]) != 0){ //the file is empty and count the # of empty file
             count_pages ++;
             arr[standard_pages] = INT_MAX;
+            cout << "out_temp_" << standard_pages << ".txt has been sorted finish." << endl;
         }
-        else fprintf(file_out, "%d\n", tmp);
-    }
+        else {
+            fprintf(file_out, "%d\n", standard); //write file
+        }
 
+    }
     for(int i = 0; i < sum_pages; i++){
-        fclose(pages[i]);
+        if(arr[i] != INT_MAX){
+            fprintf(file_out, "the last one %d\n", arr[i]);
+        }
+    }
+    // for(int i = 0; i < sum_pages; i++){
+    //     string title = "out_temp_" + to_string(i) + ".txt";
+    //     if (remove(title.c_str()) !=0)
+    //         cout<<"Remove operation failed"<<endl;
+    //     else
+    //         cout<< "out_temp_" << to_string(i) <<".txt has been removed."<<endl;
+    // }
+    for(int i = 0; i < sum_pages; i++){
+        fclose((*pages)[i]);
     }
     
-
+    cout << "finish merge" << endl;
 }
 int main(){
     FILE *file_in;
@@ -83,8 +122,9 @@ int main(){
     start = clock();
     vector<FILE*>pages;
     if(file_in){
-        pages = initial_split(file_in, block_size, &count_pages);
-        merge_file(file_out , block_size, count_pages, pages);
+        initial_split(file_in, block_size, &count_pages, &pages);
+        //fprintf(pages[0], "hello worold");
+        merge_file(file_out , block_size, count_pages, &pages);
     }
     else{
         cout << "file can not open" << endl;
